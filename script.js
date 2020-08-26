591cfe0f7343b5f17d331825083635fbd997d504
Christina <67764305+clukasko@users.noreply.github.com>
2020-08-26 12:42:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var css= document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2= document.querySelector(".color2");
+var body= document.querySelector(".gradient");
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+var generator = document.querySelector("h4");
+
+
+function setGradient() {
+body.style.background = 
+"linear-gradient(to right,"
++color1.value 
++"," 
++color2.value
++")";
+
+css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener ("input", setGradient);
+
+color2.addEventListener ("input", setGradient);
+
+
+
+
+
